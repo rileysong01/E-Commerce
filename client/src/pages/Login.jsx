2a3cc6a29decf8106a612f4077570ef400380b97
rileysong01 <rsong47@uwo.ca>
2023-11-15 13:51:56 -0500

    added styling to signup and login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 import decode from 'jwt-decode';
+import { Container, Form, Button } from 'react-bootstrap';
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -34,40 +35,53 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-
+    <Container className="my-1">
       <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-2">
+          <Form.Label>Email address:</Form.Label>
+          <Form.Control
             type="email"
-            id="email"
+            name="email"
+            placeholder="youremail@test.com"
             onChange={handleChange}
           />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
             type="password"
-            id="pwd"
+            name="password"
+            placeholder="******"
             onChange={handleChange}
           />
-        </div>
-        {error ? (
+        </Form.Group>
+        {error && (
           <div>
             <p className="error-text">The provided credentials are incorrect</p>
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        )}
+        <div className="d-flex justify-content-end">
+          <button type="submit"
+             style={{
+              border: '2px solid #cdb4db',
+              borderRadius: '5px',
+              padding: '10px',
+              cursor: 'pointer',
+              transition: 'background-color 0.3s ease, color 0.3s ease', 
+            }}
+            onMouseOver={(e) => {
+              e.target.style.backgroundColor = '#cdb4db';
+              e.target.style.color = 'white'; 
+            }}
+            onMouseOut={(e) => {
+              e.target.style.backgroundColor = '';
+              e.target.style.color = 'black'; 
+            }}
+            >Submit</button>
         </div>
-      </form>
-    </div>
+      </Form>
+    </Container>
   );
 }
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -31,55 +32,82 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-
+    <Container className="my-1">
       <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      <Form onSubmit={handleFormSubmit}>
+        <Row className="mb-2">
+          <Col xs={12} md={6}>
+            <Form.Group controlId="firstName">
+              <Form.Label className="text-muted small">First Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="firstName"
+                placeholder="First"
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6}>
+            <Form.Group controlId="lastName">
+              <Form.Label className="text-muted small">Last Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="lastName"
+                placeholder="Last"
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="mb-2">
+          <Col xs={12}>
+            <Form.Group controlId="email">
+              <Form.Label className="text-muted small">Email:</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="youremail@test.com"
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="mb-2">
+          <Col xs={12}>
+            <Form.Group controlId="password">
+              <Form.Label className="text-muted small">Password:</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="******"
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="flex-row flex-end">
+          <Col xs={12}>
+            <button type="submit"
+             style={{
+              border: '2px solid #cdb4db',
+              borderRadius: '5px',
+              padding: '10px',
+              cursor: 'pointer',
+              transition: 'background-color 0.3s ease, color 0.3s ease', 
+            }}
+            onMouseOver={(e) => {
+              e.target.style.backgroundColor = '#cdb4db';
+              e.target.style.color = 'white'; 
+            }}
+            onMouseOut={(e) => {
+              e.target.style.backgroundColor = '';
+              e.target.style.color = 'black'; 
+            }}
+            >Submit</button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
